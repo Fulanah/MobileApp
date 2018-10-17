e11f755bfea960143fa90874c66010eeaa93d6eb
Balqis Abd Manan <alarva36@yahoo.com>
2018-10-17 10:17:36 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 //import student class : Mizi
-let {person} = require ('./Students.js');
+let person = require ('./Students.js');
 
 //create readline module : mizi
 const readline = require('readline');
@@ -10,9 +10,39 @@ const rl = readline.createInterface({
 
 //rl.quesetion for student name : Mizi
 rl.question('Name : ', (name) => {
+    //Start here edited by Balqis (Except related with person.Name/)
+    var checkName = name.split("");
+    var i;
+       for (i =0; i < checkName.length; i++){
+            if(isNaN(checkName[i])){ //check if character (string) have numbers
+                continue; }
+                    else {(console.log("Can't recognized number. Please re-enter."));
+                }}
+                    
+                  /*  for (i=0; i < checkName.length; i++){
+                        while(!isNaN(checkName[i]) == true){
+                      //  if(isNaN(checkName[i])){ //check if character (string) have numbers
+                                rl.prompt(console.log("Can't recognized number. Please re-enter."));
+                        }
+                            
+                        };*/
+                
+
+    rl.question('Matric No. : ', (matricNo) => {
+        rl.question('Major : ', (major) =>{
+
     person.Name = name;
+    person.MatricNo = matricNo;
+    person.Major = major;
+
     console.log(`Your name is ${person.Name}`);
+    console.log(`Your Matric number is ${person.MatricNo}`);
+    console.log(`Your Major is ${person.Major}`);
+
+        });
+    });
 });
+
 
 
 
