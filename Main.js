0dad8aebe602d444e8b66bf58b931c232bb51d25
Balqis Abd Manan <alarva36@yahoo.com>
2018-10-18 13:34:39 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -8,41 +8,68 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-//rl.quesetion for student name : Mizi
+rl.prompt();
+
+/* Tirmizi */
+function alphanumeric(inputtxt)
+{ 
+var letters = /^[a-zA-Z]+$/;
+if(inputtxt.match(letters))
+{
+//alert('Your registration number have accepted : you can try another');
+//document.form1.text1.focus();
+return true;
+}
+else
+{
+//alert('Please input alphanumeric characters only');
+return false;
+}
+}
+
+
+//rl.question for student name : Mizi
+//function declared: Balqis
+function userInputName (){
 rl.question('Name : ', (name) => {
-    //Start here edited by Balqis (Except related with person.Name/)
-    var checkName = name.split("");
-    var i;
-       for (i =0; i < checkName.length; i++){
-            if(isNaN(checkName[i])){ //check if character (string) have numbers
-                continue; }
-                    else {(console.log("Can't recognized number. Please re-enter."));
-                }}
-                    
-                  /*  for (i=0; i < checkName.length; i++){
-                        while(!isNaN(checkName[i]) == true){
-                      //  if(isNaN(checkName[i])){ //check if character (string) have numbers
-                                rl.prompt(console.log("Can't recognized number. Please re-enter."));
-                        }
-                            
-                        };*/
-                
+    
+    if(alphanumeric(name)==true){
+        person.Name = name;
+        console.log(`Your name is ${person.Name}`);
+        ;
+    }else{
+        console.log('Please enter alphabets only')
+        userInputName();
+         
+    }
+}); 
 
+}
+
+userInputName();
+
+function userInputMatric (){
     rl.question('Matric No. : ', (matricNo) => {
-        rl.question('Major : ', (major) =>{
-
-    person.Name = name;
-    person.MatricNo = matricNo;
-    person.Major = major;
-
-    console.log(`Your name is ${person.Name}`);
-    console.log(`Your Matric number is ${person.MatricNo}`);
-    console.log(`Your Major is ${person.Major}`);
-
+        var checkMatric = matricNo.split("");
+       return  person.MatricNo = matricNo;
+    }); 
+}
+      
+        function userInputMajor(){
+        rl.question('Major (Information Technology/ Computer Science): ', (major) =>{
+            if(major != "Information Technology" || major != "Computer Science" ){
+                major = ("Major invalid Please re-enter:");
+            } 
+           return  person.Major = major;
         });
-    });
-});
+        }
+    
+   
 
 
+   /* console.log(`Your name is ${userInputName.person.Name}`);
+    console.log(`Your Matric number is ${userInputMatric.person.MatricNo}`);
+    console.log(`Your Major is ${userInputMajor.person.Major}`); */
+;
 
 
